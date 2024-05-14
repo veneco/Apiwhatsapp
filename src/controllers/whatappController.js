fe5d5660fd64b3aa11b40375488ff5367364b558
veneco <rvenegas199@gmail.com>
2024-05-14 00:42:03 -0400

    Update whatappController.js

--- src/controllers/whatappController.js
+++ src/controllers/whatappController.js
@@ -1,4 +1,21 @@
 const VerifyToken = (req, res) => {
+
+    try {
+        var accessToken = "JANSDAASJDASND0823123ADDASD";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.body["hub.challenge"];
+
+        if(challenge != null && token != null && token == accessToken)
+            {
+                res.sned(challenge)
+            }
+            else
+            {
+                res.status(400).send();
+            }
+    } catch (error) {
+        res.status(400).send();
+    }
     res.send("verificar token")
 
 } 
