91b8dfb4c19c807d5bb214323819d91cda1fe8ef
veneco <rvenegas199@gmail.com>
2024-05-14 17:48:00 -0400

    Update processMessage.js

--- src/shared/processMessage.js
+++ src/shared/processMessage.js
@@ -12,7 +12,7 @@ function Process(textUser, number)
     }
     else if (textUser.includes("conejo al horno"))
     {
-        var model = whatsappModel.MessageText("Un sabroso plato, te recomiendo el siguiente video donde podras encontrar mas informacion sobre los platos de conejo: https://www.youtube.com/watch?v=ZmrRYeHI-3o", number);
+        var model = whatsappModel.ConejoalHornoMessage("Un sabroso plato, te recomiendo el siguiente video donde podras encontrar mas informacion sobre los platos de conejo: https://www.youtube.com/watch?v=ZmrRYeHI-3o", number);
         models.push(model)
     } 
     else if (textUser.includes("adios"))
