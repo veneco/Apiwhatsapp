c69ca4fe4b46046a0afdf7f4610f842157466fe0
veneco <rvenegas199@gmail.com>
2024-05-14 09:43:31 -0400

    Update whatsappService.js

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -2,40 +2,45 @@ const https = require("https");
 
 function SendMessageWhatApp(textResponse, number)
 {
-    const data = JSON.stringify({
-        "messaging_product": "whatsapp",
-        "to": number,
-        "text":{
-            "body": textResponse
-        },
-        "type": "text"
-
-    });
-
-    const options = {
-        host: "graph.facebook.com",
-        path: "/v19.0/165335166668859/messages",
-        method: "POST",
-        body: data,
-        headers: 
-        {
-            "Content-Type": "applecation/json",
-            Authorization: "Bearer EAAGeUR8MUroBO76X41gD87FOc0yf4xR3gw1OTiIu2mJbYp4t8wLh3po1MVM2LVhTDC5MzuStIRjZCvdiBhPYF1aK5rg7ehbOdxxnior69kraRpu1PjmQAxiD2hdJvD2RaUzhmivI9YcWXpfa2VYjjZCqyBvqZCg9VcVUdZCaS5NX37TZCh7EiKNmiFq1JgMHj"
-        }
-    };
-
-    const req = https.request(options, res =>{
-        res.on("data", d=> {
-            process.stdout.write(d);
+    try {
+        const data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "to": number,
+            "text":{
+                "body": textResponse
+            },
+            "type": "text"
+    
         });
-    });
-
-    req.on("error", error => {
-        console.error(error);
-    });
-
-    req.write(data);
-    req.end();
+    
+        const options = {
+            host: "graph.facebook.com",
+            path: "/v19.0/165335166668859/messages",
+            method: "POST",
+            body: data,
+            headers: 
+            {
+                "Content-Type": "applecation/json",
+                Authorization: "Bearer EAAGeUR8MUroBO76X41gD87FOc0yf4xR3gw1OTiIu2mJbYp4t8wLh3po1MVM2LVhTDC5MzuStIRjZCvdiBhPYF1aK5rg7ehbOdxxnior69kraRpu1PjmQAxiD2hdJvD2RaUzhmivI9YcWXpfa2VYjjZCqyBvqZCg9VcVUdZCaS5NX37TZCh7EiKNmiFq1JgMHj"
+            }
+        };
+    
+        const req = https.request(options, res =>{
+            res.on("data", d=> {
+                process.stdout.write(d);
+            });
+        });
+    
+        req.on("error", error => {
+            console.error(error);
+        });
+    
+        req.write(data);
+        req.end();
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
 module.exports = {
