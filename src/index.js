57f04ee16e58a657392707890411fb1413bee632
veneco <rvenegas199@gmail.com>
2024-05-17 13:39:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 
+app.get("/", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>On</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse);
+  });
+
 app.use("/whatsapp", apiRouter)
 
 app.listen(PORT, ()=> {console.log("puerto : " + PORT)})
